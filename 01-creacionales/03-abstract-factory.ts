aa7aa2808b92860e2cbf5982dfb5d16e22e2acdc
Mary Katty Vicuna <mkvicunaore@gmail.com>
2025-03-13 17:01:18 -0500

    add design pattern abstract factory

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -17,3 +17,69 @@
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+interface Hamburger {
+    prepare(): void;
+}
+
+interface Drink {
+    pour(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+    prepare(): void {
+      console.log('Preparando hamburguesa de Pollo');
+    }
+}
+class BeefHamburger implements Hamburger {
+    prepare(): void {
+      console.log('Preparando hamburguesa de Res');
+    }
+}
+
+class Water implements Drink {
+    pour(): void {
+      console.log('Sirviendo un vaso de agua')
+    }
+}
+
+class Soda implements Drink {
+    pour(): void {
+      console.log('Sirviendo un vaso de gaseosa')
+    }
+}
+
+interface RestaurantFactory {
+    createHamburger(): Hamburger;
+    createDrink(): Drink;
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+  createDrink(): Drink {
+    return new Soda();
+  }
+}
+
+class HealthyRestaurantFactory implements RestaurantFactory {
+    createHamburger(): Hamburger {
+      return new ChickenHamburger();
+    }
+    createDrink(): Drink {
+      return new Water();
+    }
+  }
+
+function main( factory: RestaurantFactory) {
+    const hamburger = factory.createHamburger();
+    const drink = factory.createDrink();
+
+    hamburger.prepare();
+    drink.pour();
+}
+console.log('****** Pedido del menú *********');
+main(new FastFoodRestaurantFactory());
+
+console.log('****** Pedido del menú *********');
+main(new HealthyRestaurantFactory());
